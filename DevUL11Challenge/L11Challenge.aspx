35f5201ee79a4ace41d45b54b190eb33a9ee9b24
oliver fu <ofu997@gmail.com>
2017-08-10 21:31:11 -0700

    add a student class

---
+++ DevUL11Challenge/L11Challenge.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="L11Challenge.aspx.cs" Inherits="DevUL11Challenge.L11Challenge" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="~ PART 1 ~" />
+            <br />
+            <br />
+            <asp:Button ID="Button2" runat="server" Text="~ PART 2 ~" />
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" Text="~ PART 3 ~" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DevUL11Challenge/L11Challenge.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevUL11Challenge
+{
+	public partial class L11Challenge : System.Web.UI.Page
+	{
+
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public class Course
+		{
+			public string name { get; set; }
+			public string[] students { get; set; }
+			public string description { get; set; }
+
+			// public string student { get; set; }
+		}
+		public class Student
+		{
+			public int StudentId { get; set; }
+			public string name { get; set; }
+			public List<Course> Courses { get; set; }
+		}
+
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+			/*
+			Course course1 = new Course();
+			course1.name = "underwater basket weaving";
+			course1.students = new string[] { "bo peep", "curious george", "popeye" };
+			course1.description = "Get ready to learn different underwater basket weaving techniques! " +
+				"Scuba gear not included in tuition";
+			*/
+			/*
+			List<Course> listOfCourses = new List<Course>()
+			{
+				new Course() { name = "underwater basket weaving", students = new List<Student>(){
+					new Student(){StudentId=1, name="bo peep")}
+			}
+			*/
+		}	
+	}
+
+			listOfCourses.Add(course1);
+			foreach (Course course in listOfCourses)
+			{
+				resultLabel.Text += course.FormatDetailsForDisplay();
+
+			}
+}
